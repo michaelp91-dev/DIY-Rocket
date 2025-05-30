9d60ea14cb329c84f4a2f818ea1509d7793220e5
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2025-05-30 08:29:48 -0500

    Add files via upload

---
+++ src/js/components/BottomSticky.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+<script>
+
+export default {
+    props: {
+        unitSystem: {
+            type: String
+        }
+    },
+    data: () => ({
+        
+    }),
+    methods: {
+        test: () => {
+            alert('test')
+        }
+    },
+    computed: {
+        units: () => {
+            return 'US'
+        }
+    },
+    created() {
+        //
+    }
+}
+</script>
